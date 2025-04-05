f8b6fefbee9f8a2ba98af993c43416d76246274e
Juan Pablo <jpbriceno2001@gmail.com>
2025-04-05 03:34:50 +0000

    C - Bit manipulation

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * binary_to_uint - Converts a binary number to an unsigned int.
  * @b: A pointer to a string of 0 and 1 chars.
  *
- * Return: The converted number, or 0 if b is NULL or contains
- *         a character that is not 0 or 1.
+ * Return: The converted number, or 0 if b is NULL or contains a
+ *         character that is not 0 or 1.
  */
 unsigned int binary_to_uint(const char *b)
 {
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -2,8 +2,6 @@
 #define MAIN_H
 
 unsigned int binary_to_uint(const char *b);
-int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsigned long int *n, unsigned int index);
 int _putchar(char c);
 
 #endif /* MAIN_H */
